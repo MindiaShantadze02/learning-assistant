b8ff3047b8711bbfbfc3f3c106aa05c42a8a5c0b
Mindia Shantadze <shantadze200202@gmail.com>
2025-04-29 01:50:32 +0400

    added basic logic for handling quiz file creation on server side

--- server/index.js
+++ server/index.js
@@ -4,23 +4,33 @@ const fs = require('fs');
 const path = require('path');
 require('dotenv').config();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.json("Hello world");
 })
 
-app.post('/', (req, res) => {
+app.post('/quizes', (req, res) => {
+    console.log(req)
     const { body } = req;
-    const { title } = body;
+    const { title, category, quizData } = body;
+    
+    const categoryPath = path.join(__dirname, `/data/quizes/${category ? category : 'default'}`);
+    
+    fs.mkdir(categoryPath, (err) => {
+        if (err) console.log('The folder already exists');
 
-    const filePath = path.join(__dirname, `${title}.json`);
+        console.log('Category folder created successfully!');
+    }); 
+    const filePath = path.join(`${categoryPath}/${title}.json`);
 
-    fs.writeFile(filePath, content, 'utf8', (err) => {
+    fs.writeFile(filePath, JSON.stringify(quizData), 'utf8', (err) => {
         if (err) {
           return res.status(500).json({ message: 'Failed to write to file', error: err });
         }
         res.status(200).json({ message: 'Content written to file successfully' });
       });
-})
+});
 
 app.listen(process.env.PORT, () => {
     console.log('Server started on port ' + process.env.PORT);
